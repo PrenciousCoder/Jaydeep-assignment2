faacfee7b08a9ba259dc22eed21987357a10a70c
Prencious Coder <devansh.panchal03@gmail.com>
2022-06-06 14:44:45 +0200

    Add files via upload

---
+++ Question105.py
@@ -0,0 +1,67 @@
+class Calculator:
+    def __init__(self,num1,num2):
+        self.num1=num1
+        self.num2=num2
+    def addition(self):
+        temp=self.num1+self.num2
+        print(temp)
+    def subtraction(self):
+        temp=self.num1-self.num2
+        print(temp)
+    def multiplication(self):
+        temp=self.num1*self.num2
+        print(temp)
+    def Division(self):
+        temp=self.num1/self.num2
+        print(temp)
+    def Quotient(self):
+        temp=self.num1//self.num2
+        print(temp)
+    def Remainder(self):
+        temp=self.num1%self.num2
+        print(temp)
+
+operation1=Calculator(100,90)
+operation1.addition()
+operation1.subtraction()
+operation1.multiplication()
+operation1.Division()
+operation1.Quotient()
+operation1.Remainder()
+
+operation2=Calculator(11234,233242)
+operation2.addition()
+operation2.subtraction()
+operation2.multiplication()
+operation2.Division()
+operation2.Quotient()
+operation2.Remainder()   
+
+operation3=Calculator(33,12)
+operation3.addition()
+operation3.subtraction()
+operation3.multiplication()
+operation3.Division()
+operation3.Quotient()
+operation3.Remainder()   
+
+operation4=Calculator(144,9032)
+operation4.addition()
+operation4.subtraction()
+operation4.multiplication()
+operation4.Division()
+operation4.Quotient()
+operation4.Remainder()   
+
+operation5=Calculator(600,99)
+operation5.addition()
+operation5.subtraction()
+operation5.multiplication()
+operation5.Division()
+operation5.Quotient()
+operation5.Remainder()   
+
+        
+                
+
+
