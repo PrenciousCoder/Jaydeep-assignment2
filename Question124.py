04f35bd7c933fb9033a94de222187395e5a888c0
Prencious Coder <devansh.panchal03@gmail.com>
2022-06-12 16:56:57 +0200

    Add files via upload

--- Question124.py
+++ Question124.py
@@ -1,15 +1,15 @@
 #Question 10 scetion2
 class Student:
     def __init__(self):
-        self.admno=input()
+        self.admno=input("Enter: ")
         self.sname=input("Enter the student name: ")
         """self.OS_marks=input("Input marks: ")
         self.CA_marks=input("Enter the marks: ")
         self.DBMS_marks=input("Enter the marks: " )"""
     def Enter_Marks(self):
-        self.OS_marks=input("Input marks: ")
-        self.CA_marks=input("Enter the marks: ")
-        self.DBMS_marks=input("Enter the marks: " )
+        self.OS_marks=int(input("Input marks: "))
+        self.CA_marks=int(input("Enter the marks: "))
+        self.DBMS_marks=int(input("Enter the marks: " ))
     def calc_total(self):
         total_marks=self.OS_marks+self.CA_marks+self.DBMS_marks
         return int(total_marks)
@@ -21,8 +21,11 @@ class Student:
         print(percent)
 inst=Student()
 inst.Enter_Marks()
-inst.calc_total()
-inst.Show_percentage(inst.calc_total())
-inst.diplay_details(inst.calc_total(),inst.Show_percentage())
+var_marks=inst.calc_total()
+print(var_marks)
+#inst.calc_total()
+print(inst.Show_percentage(int(var_marks)))
+var_percentage=inst.Show_percentage(int(var_marks))
+inst.diplay_details(var_marks,var_percentage)
 
     
